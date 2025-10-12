9e09dbf33017e766c59148c7021a0adbf25cc137
ppsplash <ppsplash13@gmail.com>
2025-10-12 21:44:17 +0200

    progress in game creation

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -3,13 +3,26 @@ from art import logo
 
 
 def user_card():
+    global card
     card = random.sample([11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10], 2)
     print(f"Your cards: {card}, current score: {sum(card)}")
 
 
+def adding_cards():
+    global c
+    c = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card.append(random.choice(c))
+    print(f"Your cards: {card}, current score: {sum(card)}")
+    if sum(card) > 21:
+        print(f"Your final hand: {card}, final score: {sum(card)}")
+        print(f"Computer#s final hand: {comp_card}, final score. {comp_card}")
+        print("You went over. You lose")
+
+
 def computer_card():
-    card = random.sample([11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10], 2)
-    print(f"Computer's first card: {card[0]}")
+    global comp_card
+    comp_card = random.choice(c)
+    print(f"Computer's first card: {comp_card}")
 
 
 start = input("Do you want to play a game of Blackjack? Type 'y' or 'n' ").lower()
@@ -18,3 +31,6 @@ if start == 'y':
     user_card()
     computer_card()
     next = input("Type 'y' to get an another card, type 'n' to pass: ").lower()
+    if next == 'y':
+        adding_cards()
+        computer_card()
