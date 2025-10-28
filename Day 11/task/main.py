0550f6514ddfb75c57a37bc66f3bfd285e53fa45
ppsplash <ppsplash13@gmail.com>
2025-10-28 11:28:15 +0100

    in progress and working on loops

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -5,7 +5,8 @@ c = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
 def user_card():
-    global my_card, total
+    global my_card
+    global total
     my_card = random.sample([11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10], 2)
     total = sum(my_card)
     print(f"Your cards: {my_card}, current score: {total}")
@@ -22,6 +23,36 @@ def adding_usercards():
     total = sum(my_card)
     print(f"Your cards: {my_card}, current score: {total}")
     print(f"Computer's first card: {comp_card}")
+    if total > 21 and my_card != [11]:
+        print("You went over. You lose :(")
+        exit()
+
+
+def adding_compcards():
+    while sum(comp_card) != 17:
+        comp_card.append(random.choice(c))
+    print(f"Your final hand: {my_card} , final score : {total}")
+    print(f"Computer's final hand: {comp_card} , final score : {sum(comp_card)}")
+    if sum(comp_card) > 21:
+        print("Opponent went over. You win:)")
+    elif sum(comp_card) < 21 and sum(comp_card) > total:
+        print("Opponent win")
+    elif sum(comp_card) < 21 and sum(comp_card) == total:
+        print("Draw")
+    elif sum(comp_card) < 21 and total > sum(comp_card):
+        print("You went over. Opponent win!!")
+
+
+def validation(total):
+    if total == 21 and my_card == [11]:
+        print(f"Your final hand: {my_card} , final score : {total}")
+        print(f"Computer's final hand: {comp_card} , final score : {sum(comp_card)}")
+        print("Win with a Blackjack:)")
+    if total > 21 and my_card == [11]:
+        my_card.remove[11]
+        my_card.append[1]
+        total = sum(my_card)
+        print(f"Your cards: {my_card}, current score: {total}")
 
 
 start = input("Do you want to play a game of Blackjack? Type 'y' or 'n' ").lower()
@@ -29,6 +60,7 @@ if start == 'y':
     print(logo)
     user_card()
     computer_card()
+    validation(total)
     repeat = input(f"Type 'y' to get another card, type 'n' to pass: ").lower()
     if repeat == 'y':
         adding_usercards()
@@ -43,8 +75,16 @@ if start == 'y':
             print(f"Type 'y' to get another card, type 'n' to pass: ")
 
         if total < 21:
-            print(f"Type 'y' to get another card, type 'n' to pass: ")
+            ans = input(f"Type 'y' to get another card, type 'n' to pass: ").lower()
+            if ans == 'y':
+                adding_usercards()
+
+            else:
+                adding_compcards()
+
         if total > 21 and my_card != [11]:
             print("You went over. You lose :(")
-    else:
+
+    while repeat == 'n':
+        adding_compcards()
         exit()
