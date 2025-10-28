303fbfc9b3d2dfbd127d688b3125b92dc1ca10ed
ppsplash <ppsplash13@gmail.com>
2025-10-28 09:38:11 +0100

    changing code for workflow

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -5,27 +5,10 @@ c = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
 def user_card():
-    global my_card
+    global my_card, total
     my_card = random.sample([11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10], 2)
-    print(f"Your cards: {my_card}, current score: {sum(my_card)}")
-
-
-def adding_cards():
-    next = input("Type 'y' to get an another card, type 'n' to pass: ").lower()
-    while next == 'y':
-        my_card.append(random.choice(c))
-        print(f"Your cards: {my_card}, current score: {sum(my_card)}")
-        next = input("Type 'y' to get an another card, type 'n' to pass: ").lower()
-    while next == 'n' and sum(comp_card) < 17:
-        comp_card.append(random.choice(c))
-        print(f"Your final hand: {my_card}, final score: {sum(my_card)}")
-        print(f"Computer's final hand: {comp_card}, final score. {comp_card}")
-
-
-def not_adding_cards():
-    print(f"Your final hand: {my_card}, final score: {sum(my_card)}")
-    print(f"Computer's final hand: {comp_card}, final score. {comp_card}")
-    print("You went over. You lose")
+    total = sum(my_card)
+    print(f"Your cards: {my_card}, current score: {total}")
 
 
 def computer_card():
@@ -34,11 +17,11 @@ def computer_card():
     print(f"Computer's first card: {comp_card}")
 
 
-def adding_comp_card():
-    if sum(my_card) == 21 and my_card == [10, 11]:
-        print(f"Your final hand: {my_card}, final score: 0 ")
-        print(f"Computer's final hand: {comp_card}, final score. {comp_card}")
-        print("Win with a Blackjack")
+def adding_usercards():
+    my_card.append(random.choice(c))
+    total = sum(my_card)
+    print(f"Your cards: {my_card}, current score: {total}")
+    print(f"Computer's first card: {comp_card}")
 
 
 start = input("Do you want to play a game of Blackjack? Type 'y' or 'n' ").lower()
@@ -46,9 +29,22 @@ if start == 'y':
     print(logo)
     user_card()
     computer_card()
-    while sum(my_card) < 21:
-        adding_cards()
-    if sum(my_card) > 21:
-        not_adding_cards()
-    if sum(my_card) == 21:
-        adding_comp_card()
+    repeat = input(f"Type 'y' to get another card, type 'n' to pass: ").lower()
+    if repeat == 'y':
+        adding_usercards()
+        if total == 21 and my_card == [11]:
+            print("You have won with Blackjack:)")
+        if total > 21 and my_card == [11]:
+            my_card.remove[11]
+            my_card.append[1]
+            total = sum(my_card)
+            print(f"Your cards: {my_card}, current score: {total}")
+        if total == 21:
+            print(f"Type 'y' to get another card, type 'n' to pass: ")
+
+        if total < 21:
+            print(f"Type 'y' to get another card, type 'n' to pass: ")
+        if total > 21 and my_card != [11]:
+            print("You went over. You lose :(")
+    else:
+        exit()
