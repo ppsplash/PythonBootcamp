4da5095fa78903b193b23be12c7d36f355674bce
ppsplash <ppsplash13@gmail.com>
2025-10-29 18:53:23 +0100

    Finally logics are working

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -14,7 +14,7 @@ def user_card():
 
 def computer_card():
     global comp_card
-    comp_card = random.choice(c)
+    comp_card = random.sample([11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10], 1)
     print(f"Computer's first card: {comp_card}")
 
 
@@ -29,18 +29,18 @@ def adding_usercards():
 
 
 def adding_compcards():
-    while sum(comp_card) != 17:
+    while sum(comp_card) < 17:
         comp_card.append(random.choice(c))
     print(f"Your final hand: {my_card} , final score : {total}")
     print(f"Computer's final hand: {comp_card} , final score : {sum(comp_card)}")
     if sum(comp_card) > 21:
         print("Opponent went over. You win:)")
-    elif sum(comp_card) < 21 and sum(comp_card) > total:
-        print("Opponent win")
-    elif sum(comp_card) < 21 and sum(comp_card) == total:
+    elif sum(comp_card) <= 21 and sum(comp_card) > total:
+        print("Opponent win!!!!. You lose")
+    elif sum(comp_card) <= 21 and sum(comp_card) == total:
         print("Draw")
-    elif sum(comp_card) < 21 and total > sum(comp_card):
-        print("You went over. Opponent win!!")
+    elif sum(comp_card) <= 21 and total > sum(comp_card):
+        print("You win!!")
 
 
 def validation(total):
@@ -53,6 +53,7 @@ def validation(total):
         my_card.append[1]
         total = sum(my_card)
         print(f"Your cards: {my_card}, current score: {total}")
+        print(f"Computer's final hand: {comp_card} , final score : {sum(comp_card)}")
 
 
 start = input("Do you want to play a game of Blackjack? Type 'y' or 'n' ").lower()
@@ -62,29 +63,10 @@ if start == 'y':
     computer_card()
     validation(total)
     repeat = input(f"Type 'y' to get another card, type 'n' to pass: ").lower()
-    if repeat == 'y':
+    while repeat == 'y':
         adding_usercards()
-        if total == 21 and my_card == [11]:
-            print("You have won with Blackjack:)")
-        if total > 21 and my_card == [11]:
-            my_card.remove[11]
-            my_card.append[1]
-            total = sum(my_card)
-            print(f"Your cards: {my_card}, current score: {total}")
-        if total == 21:
-            print(f"Type 'y' to get another card, type 'n' to pass: ")
-
-        if total < 21:
-            ans = input(f"Type 'y' to get another card, type 'n' to pass: ").lower()
-            if ans == 'y':
-                adding_usercards()
-
-            else:
-                adding_compcards()
-
-        if total > 21 and my_card != [11]:
-            print("You went over. You lose :(")
-
-    while repeat == 'n':
+        validation(total)
+        repeat = input(f"Type 'y' to get another card, type 'n' to pass: ").lower()
+    if repeat == 'n':
         adding_compcards()
         exit()
