84d2ebf441c5fe2da0bb0e289420a5b1970fdc7c
ppsplash <ppsplash13@gmail.com>
2025-11-06 13:45:11 +0100

    Finally logics are working with globally assigned value, but somehow there is a bug bcos of that and the values storage and display is getting problematic

--- Day 11/task/art.py
+++ Day 11/task/art.py
@@ -8,3 +8,31 @@ logo = r"""
       |  \/ K|                            _/ |                
       `------'                           |__/           
 """
+
+# husbands task given to me, to create an function with parameters
+
+"""def add(a, b):
+    x = a + b
+    return x
+
+
+def multiply(a, b):
+    y = a * b
+    return y
+
+
+def divide(a, b):
+    z = a / b
+    return z
+
+
+input1 = int(input("Please enter the first value to sum:"))
+input2 = int(input("Please enter the second value to sum:"))
+output1 = add(input1, input2)
+print(output1)
+input3 = int(input("Please enter the value to multiply with the previous summed values:"))
+output2 = multiply(output1, input3)
+print(output2)
+input4 = int(input("Please enter the value to divide with the previous multiplied value:"))
+output3 = divide(output2, input4)
+print(output3)"""
--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -2,32 +2,34 @@ import random
 from art import logo
 
 c = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+my_card = []
+comp_card = []
+total = 0
 
 
 def user_card():
-    global my_card
-    global total
     my_card = random.sample([11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10], 2)
-    total = sum(my_card)
-    print(f"Your cards: {my_card}, current score: {total}")
+    print(f"Your cards: {my_card}, current score: {sum(my_card)}")
+    return my_card
 
 
 def computer_card():
-    global comp_card
     comp_card = random.sample([11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10], 1)
     print(f"Computer's first card: {comp_card}")
+    return comp_card
 
 
-def adding_usercards():
+def adding_cards(my_card, comp_card):
     my_card.append(random.choice(c))
-    total = sum(my_card)
     print(f"Your cards: {my_card}, current score: {sum(my_card)}")
     print(f"Computer's first card: {comp_card}")
-    if total > 21 and 11 not in my_card:
+    if sum(my_card) > 21 and 11 not in my_card:
         print("You went over. You lose :(")
+        exit()
+    return total
 
 
-def adding_compcards():
+def adding_computer_cards(my_card, comp_card):
     while sum(comp_card) < 17:
         comp_card.append(random.choice(c))
     print(f"Your final hand: {my_card} , final score : {sum(my_card)}")
@@ -48,6 +50,11 @@ def validation(total):
         print(f"Computer's final hand: {comp_card} , final score : {sum(comp_card)}")
         print("Win with a Blackjack:)")
         exit()
+    if total == 21 and 11 in comp_card:
+        print(f"Your final hand: {my_card} , final score : {total}")
+        print(f"Computer's final hand: {comp_card} , final score : {sum(comp_card)}")
+        print("Lose with a Blackjack:)")
+        exit()
     if total > 21 and 11 in my_card:
         my_card.remove(11)
         my_card.append(1)
@@ -56,17 +63,25 @@ def validation(total):
         print(f"Computer's final hand: {comp_card} , final score : {sum(comp_card)}")
 
 
-start = input("Do you want to play a game of Blackjack? Type 'y' or 'n' ").lower()
-if start == 'y':
+def start():
     print(logo)
-    user_card()
-    computer_card()
+    my_card = user_card()
+    comp_card = computer_card()
+    total = sum(my_card)
     validation(total)
     repeat = input(f"Type 'y' to get another card, type 'n' to pass: ").lower()
     while repeat == 'y':
-        adding_usercards()
+        adding_cards(my_card, comp_card)
+        total = sum(my_card)
         validation(total)
         repeat = input(f"Type 'y' to get another card, type 'n' to pass: ").lower()
-        while repeat == 'n':
-            adding_compcards()
-            exit()
+    while repeat == 'n':
+        adding_computer_cards(my_card, comp_card)
+        total = sum(my_card)
+        validation(total)
+        exit()
+
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n' ").lower() == 'y':
+    print("\n" * 30)
+    start()
